73dd3d391de8b13e415d5752adbdbdcbd92072a3
yanzhang111 <yanz76@uci.edu>
2026-04-22 19:21:30 -0700

    1

--- Assets/Prelearning/W4Graphs/W4 activity/DialogueAdvancerW4.cs
+++ Assets/Prelearning/W4Graphs/W4 activity/DialogueAdvancerW4.cs
@@ -5,14 +5,15 @@ namespace W4Activity
 {
     public class DialogueAdvancerW4 : MonoBehaviour
     {
-        // Singleton stuff- allows this class to be accessed from anywhere in the codebase without a reference (like a member variable)
-        // do not edit this line
-        public static DialogueAdvancerW4 _Instance {get; private set;}
+      
+        public static DialogueAdvancerW4 _Instance { get; private set; }
 
-        // More Singleton stuff
-        // do not edit this method
-        private void Awake() {
-            if (_Instance != null && _Instance != this) {
+        public DialogueNodeW4 startingDialogue;
+
+        private void Awake()
+        {
+            if (_Instance != null && _Instance != this)
+            {
                 Destroy(this);
                 return;
             }
@@ -20,9 +21,12 @@ namespace W4Activity
             _Instance = this;
         }
 
-        // Button hooks up to this method
-        // do not edit this method
-        public void ChooseDialogue (DialogueNodeW4 nextLine)
+        private void Start()
+        {
+            EventBus.Trigger(VsEventNames.NewDialogueEvent, startingDialogue);
+        }
+
+        public void ChooseDialogue(DialogueNodeW4 nextLine)
         {
             EventBus.Trigger(VsEventNames.NewDialogueEvent, nextLine);
         }
